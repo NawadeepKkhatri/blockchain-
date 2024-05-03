0c4fbb114b6ab4351f63fbcbc853320168fca98a
NawadeepKkhatri <nawadeep.khatri@proton.me>
2024-05-02 21:55:45 -0600

    tf

--- noobieblock/src/Main.java
+++ noobieblock/src/Main.java
@@ -7,7 +7,7 @@ import com.google.gson.GsonBuilder;
 public class Main {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    public static int difficulty = 10;
+    public static int difficulty = 5;
 
     public static void main(String[] args) {
         //add our blocks to the blockchain ArrayList:
